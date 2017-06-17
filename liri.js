bfece092cc7aa196af245ecd73dacd1c8feecdbc
hockey4life63 <Amelancon68@gmail.com>
2017-06-16 17:23:02 -0700

    initial setup

---
+++ liri.js
@@ -0,0 +1,20 @@
+let keys = require("./keys")
+
+
+let liri = {}
+
+liri["my-tweets"] = function() {
+    /* body... */
+}
+
+liri["spotify-this-song"] = function(argument) {
+    /* body... */
+}
+
+liri["movie-this"] = function(argument) {
+    /* body... */
+}
+
+liri["do-what-it-says"] = function(argument) {
+    /* body... */
+}
